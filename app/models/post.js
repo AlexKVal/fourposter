d52f4d3b24fa848afba813526501bc08c31f9395
AlexKVal <alexkval@gmail.com>
2015-10-31 11:50:15 +0200

    [changed] styling and added button 'Delete' scaffolding

--- app/models/post.js
+++ app/models/post.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   author: DS.attr('string'),
-  createDate: DS.attr('date'),
+  createdDate: DS.attr('date'),
   text: DS.attr('string')
 });
